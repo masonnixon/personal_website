c0a7afcfc7b491b3afad0293a299e8c02c03d735
masonnixon <masonnixon@gmail.com>
2012-08-23 18:14:26 -0400

    Added all files

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ personal_website
 
 Personal Website
 
-Added this line to try and get html page to show up online.
+08-23-2012
+To access page online, the default GitHub address is http://masonnixon.github.com/personal_website.
+
+I added the only version of my page I had on hand from Db which is dated 02-04-2012.
---
+++ highslide/highslide.config.js
@@ -0,0 +1,35 @@
+/**
+*	Site-specific configuration settings for Highslide JS
+*/
+hs.graphicsDir = 'highslide/graphics/';
+hs.outlineType = 'custom';
+hs.captionEval = 'this.a.title';
+
+
+// Add the slideshow controller
+hs.addSlideshow({
+	slideshowGroup: 'group1',
+	interval: 5000,
+	repeat: false,
+	useControls: true,
+	fixedControls: 'fit',
+	overlayOptions: {
+		opacity: 0.75,
+		position: 'bottom center',
+		offsetX: 0,
+		offsetY: -10,
+		hideOnMouseOut: true
+	},
+	thumbstrip: {
+		mode: 'horizontal',
+		position: 'bottom center',
+		relativeTo: 'viewport'
+	}
+
+});
+
+// gallery config object
+var config1 = {
+	slideshowGroup: 'group1',
+	transitions: ['expand', 'crossfade']
+};
